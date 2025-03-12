8bbb0475f419a6314916e66365647d635aeff5e1
Rvlp <rafaelpoi524@gmail.com>
2025-03-12 10:40:52 -0300

    Update AnoBissexto.py

--- AnoBissexto.py
+++ AnoBissexto.py
@@ -1,4 +1,5 @@
-ano = int(input('Digite o ano: '))
+#Código mais complicado de entender pra mim, principalmente a lógica por trás e a linguagem de != 0 significar que não é dividido.
+ano = int(input('Digite o ano: ')) 
 if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
     print('Esse é um ano bissexto')
 else:
