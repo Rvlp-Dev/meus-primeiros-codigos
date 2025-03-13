5b47ddb52d924069e1970e13f2a00dad9b047dc2
Rvlp <rafaelpoi524@gmail.com>
2025-03-13 10:14:09 -0300

    Update códigoIMC.py

--- códigoIMC.py
+++ códigoIMC.py
@@ -1,10 +1,11 @@
+#Exercico para ir práticando um pouco do ques estou aprendendo 
 peso = float(input('Informe seu peso: '))
 altura = float(input('Informe sua altura: '))
 imc = peso / altura ** 2
 print(f'seu Imc é {imc:.2f}')
 if imc < 18.5:
 	print('Abaixo do peso')
-elif imc >= 18.5 and imc <= 24.9:
+elif imc >= 18.5 and imc <= 24.9: #Aqui o idela seria colocar 25 pois daria um erro caso o resultado desse 24.95.
 	print('Peso normal')
 elif imc >= 25 and imc <= 29.9:
 	print('Sobrepeso')
